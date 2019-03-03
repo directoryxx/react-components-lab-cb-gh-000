c1dfccab558541bead8a96dd58386f0eebdc41f5
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2019-03-03 06:37:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,11 +12,18 @@ const BUTCHER_PRODUCTS = [
 export class OlderCoaster extends Component {
 
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
+  render() {
+     return (
+       <div class="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+     )
+   }
   //
 }
 
