400eef321cf1c3c25495b2a8a973e42dd723e231
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2019-03-03 06:39:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -43,14 +43,15 @@ export class ButcherShop extends Component {
   // your code here
   render() {
      return (
-       <div class="oldercoaster">
-        <p>Two grannies having the time of their life!</p>
-        <p>Passengers:</p>
-        <ul>
-          <li>Agnes</li>
-          <li>Muriel</li>
-        </ul>
-      </div>
+       <div class="butcher-shop">
+  <p>Hello! We have the following products for sale today:</p>
+  <ul>
+    <li>Tenderloin</li>
+    <li>Short ribs</li>
+    <li>Beef shin</li>
+    <li>Ribeye</li>
+  </ul>
+</div>
      )
    }
 }
